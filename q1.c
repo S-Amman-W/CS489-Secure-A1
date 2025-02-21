c361803d821b4dfd7f9dc026130f1ca3dae77cbd
Amman Waheed <waheed2002.sw@gmail.com>
2025-02-21 18:53:02 +0000

    new code

--- q1.c
+++ q1.c
@@ -1,8 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-    
+#include <ctype.h>
+
 #define BUFSIZE 256
-    
+
+// Helper function to sanitize filename
+void sanitize_filename(const char* input, char* output, size_t outsize) {
+    size_t i;
+    for (i = 0; input[i] && i < outsize - 1; i++) {
+        // Only allow alphanumeric chars, dots, underscores, and hyphens
+        if (isalnum(input[i]) || input[i] == '.' || input[i] == '_' || input[i] == '-') {
+            output[i] = input[i];
+        } else {
+            output[i] = '_';  // Replace unsafe chars with underscore
+        }
+    }
+    output[i] = '\0';  // Ensure null termination
+}
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -11,8 +26,9 @@ int main(int argc, char** argv) {
         return -1;
     }
     char cmd[BUFSIZE] = "wc -c < ";
-    char fileName[1000] = {0};
-    encodeShellString(fileName, 1000, argv[1]); 
-    strcat(cmd, argv[1]);
-    system(cmd);
+    char safe_filename[BUFSIZE];
+    sanitize_filename(argv[1], safe_filename, BUFSIZE);
+    char cmd2[BUFSIZE] = {0};
+    sprintf(cmd2, cmd, safe_filename);
+    system(cmd2);
 }
