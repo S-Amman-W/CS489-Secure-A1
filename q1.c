01ed0c017aabf48f9daf2334c0934c4fde4f3b9c
Amman Waheed <waheed2002.sw@gmail.com>
2025-02-21 19:16:58 +0000

    new code 4

--- q1.c
+++ q1.c
@@ -1,35 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define BUFSIZE 256
+#define BUFFSIZE 256
 
-// Helper function to sanitize filename
-void sanitize_filename(const char* input, char* output, size_t outsize) {
-    size_t i;
-    for (i = 0; input[i] && i < outsize - 1; i++) {
-        // Only allow alphanumeric chars, dots, underscores, and hyphens
-        if (isalnum(input[i]) || input[i] == '.' || input[i] == '_' || input[i] == '-') {
-            output[i] = input[i];
-        } else {
-            output[i] = '_';  // Replace unsafe chars with underscore
-        }
-    }
-    output[i] = '\0';  // Ensure null termination
-}
-
-// This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
+    FILE* ptr = fopen(argv[1], "r");
+    
+    if (ptr == NULL) {
+        printf("File cannot be opened.\n");
+        return(-1);
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    char safe_filename[BUFSIZE];
-    sanitize_filename(argv[1], safe_filename, BUFSIZE);
-    char cmd2[BUFSIZE] = {0};
-    strncat(cmd, safe_filename, BUFSIZE);
-    system(cmd);
+    
+    char buff[BUFFSIZE];
+    
+    while (fgets(buff, BUFFSIZE, ptr) != NULL) {
+        printf("%s", buff);
+    }
+        
+    return 0; 
 }
