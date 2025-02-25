42b433ea788b0ae50003de7d71bcc63f90e7d427
Amman Waheed <waheed2002.sw@gmail.com>
2025-02-25 16:42:03 +0000

    a2 code

--- q1.c
+++ q1.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define BUFFSIZE 256
-
-int main(int argc, char** argv) {
-    FILE* ptr = fopen(argv[1], "r");
     
-    if (ptr == NULL) {
-        printf("File cannot be opened.\n");
-        return(-1);
-    }
-    
-    char buff[BUFFSIZE];
-    
-    while (fgets(buff, BUFFSIZE, ptr) != NULL) {
-        printf("%s", buff);
-    }
-        
-    return 0; 
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index-1]);
 }
